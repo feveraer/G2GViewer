6301a19d9724eda43886e7caaa27c996e634ed51
feveraer <frederic.everaert@gmail.com>
2014-06-30 00:48:57 +0200

    Clicking on a movie title starts a new activity. A movie activity shows the html body text of that link.

--- app/src/main/java/com/android/g2gviewer/MovieActivity.java
+++ app/src/main/java/com/android/g2gviewer/MovieActivity.java
@@ -1,5 +1,6 @@
 package com.android.g2gviewer;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Intent;
@@ -7,31 +8,32 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 
 import java.io.IOException;
 
+public class MovieActivity extends Activity {
 
-public class MainActivity extends Activity {
-
-    private LinearLayout linearLayout;
-    public final static String LINK_G2G = "http://g2g.fm/";
     private ProgressDialog progressDialog;
-    public final static String EXTRA_MESSAGE = "com.android.g2gviewer.MESSAGE";
+    private String movieLink;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        linearLayout = (LinearLayout) findViewById(R.id.main_linear_layout);
+        setContentView(R.layout.activity_movie);
+
+        Intent intent = getIntent();
+        String[] movieRef = intent.getStringArrayExtra(MainActivity.EXTRA_MESSAGE);
+        textView = (TextView) findViewById(R.id.movie_textview);
+        ActionBar ab = getActionBar();
+        ab.setTitle(movieRef[0]);
+        movieLink = MainActivity.LINK_G2G + movieRef[1];
+        textView.setText(movieLink);
+
         DownloadTask task = new DownloadTask();
         task.execute();
     }
@@ -40,7 +42,7 @@ public class MainActivity extends Activity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        getMenuInflater().inflate(R.menu.movie, menu);
         return true;
     }
 
@@ -61,7 +63,7 @@ public class MainActivity extends Activity {
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
-            progressDialog = new ProgressDialog(MainActivity.this);
+            progressDialog = new ProgressDialog(MovieActivity.this);
             progressDialog.setTitle(getString(R.string.progress_wait));
             progressDialog.setMessage(getString(R.string.progress_loading));
             progressDialog.setIndeterminate(false);
@@ -71,23 +73,12 @@ public class MainActivity extends Activity {
         @Override
         protected Void doInBackground(Void... urls) {
             try {
-                Document document = Jsoup.connect(LINK_G2G).get();
-                final Elements movieLinks = document.select(".topic_head > a[href]");
+                final Document document = Jsoup.connect(movieLink).get();
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-                        for (final Element e : movieLinks) {
-                            Button btnMovie = new Button(MainActivity.this);
-                            btnMovie.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
-                            btnMovie.setText(e.text());
-                            btnMovie.setOnClickListener(new View.OnClickListener() {
-                                @Override
-                                public void onClick(View view) {
-                                    goToMovie(view, e.attr("href"));
-                                }
-                            });
-                            linearLayout.addView(btnMovie);
-                        }
+                        textView.setText(textView.getText().toString() + "\n\n"
+                                + document.body().text());
                     }
                 });
             } catch (IOException e) {
@@ -99,13 +90,6 @@ public class MainActivity extends Activity {
         @Override
         protected void onPostExecute(Void result) {
             progressDialog.dismiss();
-        }
-
-        public void goToMovie(View view, String link) {
-            Intent intent = new Intent(MainActivity.this, MovieActivity.class);
-            Button b = (Button) view;
-            intent.putExtra(EXTRA_MESSAGE, new String[]{b.getText().toString(), link});
-            startActivity(intent);
         }
     }
 }
